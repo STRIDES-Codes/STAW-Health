a1772a80e15a23d0731ab5e33b59704d6f8680bd
Mark Rustad <markr@spatial-forecasting.us-east4-c.c.nih-cit-csfr-training-test1.internal>
2021-06-23 16:40:44 +0000

    update clean_data.R, clean cdc raw_data files
    scripts/clean_data.R now includes a function definition to clean individual text file, then it recursively applies that function to the entire directory.

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -3,14 +3,37 @@
 # load packages
 library(tidyverse)
 
-temp <- read_tsv("~/Spatial-forecasting-of-environmental-infectious-diseases-within-vulnerable-populations/data/cdc/raw_data/lyme/2019-table2j.txt",
-         col_names = FALSE,
-         skip_empty_rows = FALSE)
+# function to clean an individual text file
+clean_files <- function(file) {
+  # import each line as string, lines is character vector
+  lines <- read_lines(file, skip_empty_rows = FALSE, progress = interactive())
+  
+  # temp stores lines as dataframe
+  temp <- lines %>% tibble()
+  names(temp) = "lines"
+  temp %<>% rowwise() %>% mutate(lines = str_replace_all(lines, "\x97", "NA"))
+  
+  # get row numbers of data and select
+  ind_1 <- which(temp == "tab delimited data:")
+  ind_2 <- which(str_starts(temp$lines, "U.S. Virgin Islands"))
+  data <- temp[(ind_1+1):ind_2,1]
+  
+  # list of column names
+  colnames <- temp[4:(ind_1-2),1] %>%
+    rowwise() %>% mutate(lines = str_replace_all(lines, "<sup>NA</sup>", ""), lines = trimws(lines))
+  
+  # separate column to columns
+  data_s <- data %>% separate(col = lines, sep = "\t", into = letters[1:nrow(colnames)])
+  names(data_s) <- deframe(colnames)
+  
+  return(data_s)
+}
 
-# ind_col1 <- which(temp$X1 == "column labels in same order that data fields appears in each record below:")
+# file to clean
+filelist <- list.files(path = "~/Spatial-forecasting-of-environmental-infectious-diseases-within-vulnerable-populations/data/cdc/raw_data/",
+                       recursive = TRUE,
+                       full.names = TRUE)
 
-ind_1 <- which(temp$X1 == "tab delimited data:")
-ind_2 <- which(temp$X1 == "U.S. Virgin Islands")
-
-df <- read_tsv("~/Spatial-forecasting-of-environmental-infectious-diseases-within-vulnerable-populations/data/cdc/raw_data/lyme/2019-table2j.txt",
-               skip = 15, col_names = FALSE, skip_empty_rows = FALSE)
+# list of dataframes
+df_list <- lapply(filelist, clean_files)
+names(df_list) <- filelist
